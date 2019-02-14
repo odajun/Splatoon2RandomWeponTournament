40c043ccdf7a24a99601dd93703c368824fde760
demodemojun <g.odajun@gmail.com>
2019-02-14 23:58:11 +0900

    fix typo

--- randomizer/randomizer.py
+++ randomizer/randomizer.py
@@ -24,59 +24,59 @@ def readFileToList(path):
 	f.close()
 	return list
 
-def choiceUserWepon(wepon_set, user_wepon_list, num):
-    duplicated_wepon = True
-    wepon = ""
-    while duplicated_wepon :
-        wepon = random.choice(user_wepon_list)
-        duplicated_wepon = False
-        for selected_wepon_line in wepon_set :
-            if selected_wepon_line[num] == wepon :
-                duplicated_wepon = True
+def choiceUserweapon(weapon_set, user_weapon_list, num):
+    duplicated_weapon = True
+    weapon = ""
+    while duplicated_weapon :
+        weapon = random.choice(user_weapon_list)
+        duplicated_weapon = False
+        for selected_weapon_line in weapon_set :
+            if selected_weapon_line[num] == weapon :
+                duplicated_weapon = True
                 break
-    return wepon
+    return weapon
 
-def choiceTeamWepon(wepon_set, user_wepon_list):
-    wepon_line = []
-    for i in range(len(user_wepon_list)) :
-        duplicated_wepon = True
-        while duplicated_wepon :
-            wepon = choiceUserWepon(wepon_set, user_wepon_list[i], i)
-            if wepon not in wepon_line :
-                duplicated_wepon = False
-        wepon_line.append(wepon)
-    return wepon_line
+def choiceTeamweapon(weapon_set, user_weapon_list):
+    weapon_line = []
+    for i in range(len(user_weapon_list)) :
+        duplicated_weapon = True
+        while duplicated_weapon :
+            weapon = choiceUserweapon(weapon_set, user_weapon_list[i], i)
+            if weapon not in weapon_line :
+                duplicated_weapon = False
+        weapon_line.append(weapon)
+    return weapon_line
 
-def getWeponTypeList():
-    wepon_set = []
-    wepon_type_list = ["blaster", "charger", "manuver", "roller_brush", "shelter", "shooter", "shooter2", "slosher", "splatling"]
-    user_wepon_list = []
+def getweaponTypeList():
+    weapon_set = []
+    weapon_type_list = ["blaster", "charger", "manuver", "roller_brush", "shelter", "shooter", "shooter2", "slosher", "splatling"]
+    user_weapon_list = []
     for i in range(4) :
-        user_wepon_list.append(copy.deepcopy(wepon_type_list))
+        user_weapon_list.append(copy.deepcopy(weapon_type_list))
 
     # 1週目はランダムに選ぶ
-    wepon_set.append(random.sample(wepon_type_list, 4))
+    weapon_set.append(random.sample(weapon_type_list, 4))
     # 選んだ武器種を候補リストから削除
-    for i in range(len(user_wepon_list)) :
-        user_wepon_list[i].remove(wepon_set[0][i])
+    for i in range(len(user_weapon_list)) :
+        user_weapon_list[i].remove(weapon_set[0][i])
 
     # 2週目以降
     for i in range(1, 5):
-        wepon_line = choiceTeamWepon(wepon_set, user_wepon_list)
-        for j in range(len(user_wepon_list)) :
-            user_wepon_list[j].remove(wepon_line[j])
-        wepon_set.append(wepon_line)
-    return wepon_set
+        weapon_line = choiceTeamweapon(weapon_set, user_weapon_list)
+        for j in range(len(user_weapon_list)) :
+            user_weapon_list[j].remove(weapon_line[j])
+        weapon_set.append(weapon_line)
+    return weapon_set
 
-def choiceWepon(wepon_type):
-    wepon_set = []
-    for i in wepon_type :
-        wepon_list = []
+def choiceweapon(weapon_type):
+    weapon_set = []
+    for i in weapon_type :
+        weapon_list = []
         for j in i :
-            path = "./wepons/" + j + "_file.txt"
-            wepon_list.append(random.choice(readFileToList(path)))
-        wepon_set.append(wepon_list)
-    return wepon_set
+            path = "./weapons/" + j + "_file.txt"
+            weapon_list.append(random.choice(readFileToList(path)))
+        weapon_set.append(weapon_list)
+    return weapon_set
 
 def init_result():
     global result_set
@@ -85,16 +85,16 @@ def init_result():
     path = "./stage.txt"
     stage_list = readFileToList(path)
     random.shuffle(rule_list)
-    wepon_set = getWeponTypeList()
-    alpha_wepon_type = getWeponTypeList()
-    bravo_wepon_type = getWeponTypeList()
+    weapon_set = getweaponTypeList()
+    alpha_weapon_type = getweaponTypeList()
+    bravo_weapon_type = getweaponTypeList()
 
-    alpha_wepon_list = choiceWepon(alpha_wepon_type)
-    bravo_wepon_list = choiceWepon(bravo_wepon_type)
+    alpha_weapon_list = choiceweapon(alpha_weapon_type)
+    bravo_weapon_list = choiceweapon(bravo_weapon_type)
 
     result_set.append(rule_list)
-    result_set.append(alpha_wepon_list)
-    result_set.append(bravo_wepon_list)
+    result_set.append(alpha_weapon_list)
+    result_set.append(bravo_weapon_list)
     result_set.append(random.sample(stage_list,5))
     return
 
@@ -112,10 +112,10 @@ def setTeamMember(name):
     path = "./teams/" + name + ".txt"
     return readFileToList(path)
 
-def getOutputText(wepon_list, member_list):
+def getOutputText(weapon_list, member_list):
     text = ""
     for i in range(len(member_list)) :
-        text = text + member_list[i] + " : " + wepon_list[i] + "\n"
+        text = text + member_list[i] + " : " + weapon_list[i] + "\n"
     return text
 
 def getResultSet():
@@ -133,28 +133,28 @@ def getBravoMembers():
 def is_unique(seq):
     return len(seq) == len(set(seq))
 
-def weponCheck(wepon_set):
+def weaponCheck(weapon_set):
     text = ""
-    for i in range(len(wepon_set)) :
+    for i in range(len(weapon_set)) :
         text = text + str(i+1) + "ゲーム目の武器種の組み合わせ"
-        if is_unique(wepon_set[i]) :
+        if is_unique(weapon_set[i]) :
             text = text + "は重複なし\n"
         else :
             text = text + "に重複を確認\n"
-            text = text + ",".join(wepon_set[i]) + "\n"
+            text = text + ",".join(weapon_set[i]) + "\n"
 
-    user_wepon_set = [[],[],[],[]]
-    for i in range(len(wepon_set)) :
-        for j in range(len(user_wepon_set)) :
-            user_wepon_set[j].append(wepon_set[i][j])
+    user_weapon_set = [[],[],[],[]]
+    for i in range(len(weapon_set)) :
+        for j in range(len(user_weapon_set)) :
+            user_weapon_set[j].append(weapon_set[i][j])
         
-    for i in range(len(user_wepon_set)) :
+    for i in range(len(user_weapon_set)) :
         text = text + str(i+1) + "人目の武器種の組み合わせ"
-        if is_unique(user_wepon_set[i]): 
+        if is_unique(user_weapon_set[i]): 
             text = text + "は重複なし\n"
         else :
             text = text + "に重複を確認\n"
-            text = text + ",".join(user_wepon_set[i]) + "\n"
+            text = text + ",".join(user_weapon_set[i]) + "\n"
 
     return text
         
@@ -172,8 +172,8 @@ if debug :
     pprint(setBravoMember)
     pprint(result_set)
 
-    print(weponCheck(result_set[1]))
-    print(weponCheck(result_set[2]))
+    print(weaponCheck(result_set[1]))
+    print(weaponCheck(result_set[2]))
     sys.exit()
     
 
@@ -196,8 +196,8 @@ async def on_message(message):
                 setBravoMember(contents[2])
                 init_result()
                 result_set = getResultSet()
-                print(weponCheck(result_set[1]))
-                print(weponCheck(result_set[2]))
+                print(weaponCheck(result_set[1]))
+                print(weaponCheck(result_set[2]))
                 reply = 'ランダマイザーの内容をリセットしました'
                 await client.send_message(message.channel, reply)
 
